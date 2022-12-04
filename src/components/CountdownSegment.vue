380d49fc7f2ef56de0880a7274626290335a1e81
Rapol <vuthavyrapol@gmail.com>
2022-12-04 17:13:34 +0700

    Finish #3

--- src/components/CountdownSegment.vue
+++ src/components/CountdownSegment.vue
@@ -7,7 +7,9 @@ defineProps({
 <template>
   <div class="segment text-center">
     <div class="pt-10 overflow-hidden relative">
-      <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      <Transition>
+        <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      </Transition>
     </div>
 
     <span class="label block pt-2">{{ label }}</span>
@@ -23,5 +25,19 @@ defineProps({
 }
 .label {
   font-size: 16px;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+.v-enter-from {
+  transform: translateY(-100%) translateX(-50%);
+}
+.v-leave-to {
+  transform: translateY(100%) translateX(-50%);
+}
+.v-enter-to,
+.v-leave-from {
+  transform: translateY(0px) translateX(-50%);
 }
 </style>
